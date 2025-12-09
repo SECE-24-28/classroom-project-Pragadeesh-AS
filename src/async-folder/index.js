f8acac93d3ef08dd041d7266322d16ec595845c9
Pragadeesh.A.S <aspragadeesh75@gmail.com>
2025-12-09 09:13:45 +0530

    DAY_7-ASYNC

---
+++ src/async-folder/index.js
@@ -0,0 +1,12 @@
+// console.log("Start");
+// for(let i=0;i<1e9;i++){
+//     console.log("Hello World");
+// }
+// console.log("end");
+
+// Syncronous
+
+console.log("Start");
+setTimeout(() => {
+    console.log("Pragadeesh ");
+},2000)
