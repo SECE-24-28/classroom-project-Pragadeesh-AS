ef62a6661f79b3b6a055893fe57142895d3de41f
Pragadeesh.A.S <aspragadeesh75@gmail.com>
2025-12-11 14:15:00 +0530

    Created get user Details

--- apis-function/user-functions.js
+++ apis-function/user-functions.js
@@ -1,8 +1,9 @@
+
 const User = require("../models/user");
 exports.createUser = async (req, res) => {
   try {
     const { firstName, email } = req.body;
-    await User.insertOne({
+    const creatUser = await User.insertOne({
       firstName: firstName,
       email: email,
     });
@@ -20,11 +21,8 @@ exports.createUser = async (req, res) => {
 
 exports.createManyUsers = async (req, res) => {
   try {
-    const { firstName, email } = req.body;
-    const createUser = await User.insertOne({
-       firstName: firstName,
-      email: email,
-    });
+    const { details } = req.body;
+    const createUser = await User.insertMany(details);
     return res.status(200).json({
       success: true,
       message: "User is created successfully",
@@ -61,6 +59,89 @@ exports.updateEmail = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "email is updated",
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+
+exports.getDetails = async (req, res) => {
+  try {
+    const { id, mainDetails } = req.query;
+    const { data } = req.params;
+    console.log("the params:", data);
+
+    // console.log("The id details:", id, mainDetails);
+    // console.log("=>(The data:)<=", data);
+    // console.log("The main details:", mainDetails);
+    return res.status(200).json({
+      success: true,
+      message: "The data got filled",
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+exports.userRegister = async (req, res) => {
+  try {
+    const { email, firstName, secondName, mobileNumber, password } = req.body;
+
+    const findUser = await User.findOne({
+      email: email,
+      mobileNumber: mobileNumber,
+    });
+    //console.log("The find user:", findUser);
+
+    if (findUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already resigtered",
+      });
+    }
+    await User.create({
+      email,
+      firstName,
+      secondName,
+      mobileNumber,
+      password,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "User is registed successfully",
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+
+exports.getAllUserDetails = async (req, res) => {
+  try {
+    console.log("HEllo world");
+
+    const { email } = req.params;
+    console.log("The request query:", req.query);
+
+    console.log("The email query:", email);
+
+    const getDetails = await User.find({ email });
+    if (!getDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "User not existing",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: getDetails,
     });
   } catch (e) {
     res.status(404).json({
--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -4,10 +4,16 @@ const {
   createManyUsers,
   getAllUsers,
   updateEmail,
+  getDetails,
+  userRegister,
+  getAllUserDetails,
 } = require("../apis-function/user-functions");
 const router = express.Router();
-router.post("/userSignup", createUser);
+router.post("/userSignup", createUser); 
 router.post("/userSignupMany", createManyUsers);
 router.get("/getUsers", getAllUsers);
 router.put("/modifyMail", updateEmail);
+router.get("/get-details/:id", getDetails);
+router.post("/userRegister", userRegister);
+router.get("/getUserDetails/:email", getAllUserDetails);
 module.exports = router;
